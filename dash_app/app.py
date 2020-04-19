da6569755da8f37b1291ba385d1c0c8a0eea4755
Vaclav Sisl <vaclav.sisl@gmail.com>
2020-04-19 12:10:48 +0200

    using Bootstrap css only

--- dash_app/app.py
+++ dash_app/app.py
@@ -8,8 +8,8 @@ from io import BytesIO
 # external_stylesheets = [dbc.themes.BOOTSTRAP, 'https://codepen.io/chriddyp/pen/bWLwgP.css']
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.BOOTSTRAP]
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# external_stylesheets = [dbc.themes.BOOTSTRAP]
-external_stylesheets = [dbc.themes.GRID, 'https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+# external_stylesheets = [dbc.themes.GRID, 'https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'Covid-19 v okresech'
