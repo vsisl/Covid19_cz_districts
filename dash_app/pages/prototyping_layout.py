818486448e1f6ef621f5f54e79bc8f19a539980a
Vaclav Sisl <vaclav.sisl@gmail.com>
2020-04-19 11:41:23 +0200

    removed unnecessary imports

--- dash_app/pages/prototyping_layout.py
+++ dash_app/pages/prototyping_layout.py
@@ -2,9 +2,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_table
-import pandas as pd
 
-from dash_app.app import app, districts, df_table, decimal_format
+from dash_app.app import df_table, decimal_format
 
 # --- DEFINE LAYOUT
 row_graph = dbc.Row([
