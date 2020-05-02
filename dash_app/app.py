0dce9089a26888b1759589a3a08daba82e0dd9b5
Vaclav Sisl <vaclav.sisl@gmail.com>
2020-05-02 22:03:24 +0200

    try-except clause for data query

--- dash_app/app.py
+++ dash_app/app.py
@@ -17,36 +17,41 @@ server = app.server
 app.config.suppress_callback_exceptions = True
 decimal_format = Format(precision=2, scheme=Scheme.decimal)
 offline_data_mode = False
+PATHS_TO_DB_FILES = {'dff': 'data/dff.pkl'}
 
 if offline_data_mode:
     # offline data acquisition
-    PATHS_TO_DB_FILES = {'dff': 'data/dff.pkl'}
     dff = pd.read_pickle(PATHS_TO_DB_FILES['dff'])
 
 else:
     # online data acquisition
-    link = 'https://docs.google.com/spreadsheets/d/1FFEDhS6VMWon_AWkJrf8j3XxjZ4J6UI1B2lO3IW-EEc/export?format=csv&id=1FFEDhS6VMWon_AWkJrf8j3XxjZ4J6UI1B2lO3IW-EEc&gid=1011737151'
-    r = requests.get(link)
-    data = r.content
-    # print('-- DATA QUERY EXECUTED --')
+    try:
+        link = 'https://docs.google.com/spreadsheets/d/1FFEDhS6VMWon_AWkJrf8j3XxjZ4J6UI1B2lO3IW-EEc/export?format=csv&id=1FFEDhS6VMWon_AWkJrf8j3XxjZ4J6UI1B2lO3IW-EEc&gid=1011737151'
+        r = requests.get(link)
+        data = r.content
+        # print('-- DATA QUERY EXECUTED --')
 
-    # get data in wide format
-    df = pd.read_csv(BytesIO(data))
-    df = df.set_index(['Kraj', 'Okres']).T
-    df = df.dropna()
-    # df = df.drop(index='Kontrola')
-    # df = df.drop(index='Změna')
-    df.index.rename(name='Datum', inplace=True)
+        # get data in wide format
+        df = pd.read_csv(BytesIO(data))
+        df = df.set_index(['Kraj', 'Okres']).T
+        df = df.dropna()
+        # df = df.drop(index='Kontrola')
+        # df = df.drop(index='Změna')
+        df.index.rename(name='Datum', inplace=True)
 
-    # wide to long
-    dff = df.stack(level=['Kraj', 'Okres'])
-    dff = pd.DataFrame(dff)
-    dff.reset_index(inplace=True)
-    dff.rename(columns={0: 'Nakažení'}, inplace=True)
+        # wide to long
+        dff = df.stack(level=['Kraj', 'Okres'])
+        dff = pd.DataFrame(dff)
+        dff.reset_index(inplace=True)
+        dff.rename(columns={0: 'Nakažení'}, inplace=True)
 
-    # explicitly setting types
-    dff['Datum'] = dff['Datum'].astype('datetime64')
-    dff['Nakažení'] = dff['Nakažení'].astype('int64')
+        # explicitly setting types
+        dff['Datum'] = dff['Datum'].astype('datetime64')
+        dff['Nakažení'] = dff['Nakažení'].astype('int64')
+
+    except:
+        # offline data acquisition
+        dff = pd.read_pickle(PATHS_TO_DB_FILES['dff'])
 
 # prepare df for table
 df1 = dff[dff.Datum == dff.Datum.max()]
