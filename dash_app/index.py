a341f03d9884382c18f5ff1edabd221b05107233
Vaclav Sisl <vaclav.sisl@gmail.com>
2020-05-02 22:26:10 +0200

    removed unused import

--- dash_app/index.py
+++ dash_app/index.py
@@ -2,7 +2,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
-import socket
 import os
 import sys
 
