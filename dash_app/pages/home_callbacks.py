39a17cd76a24eaa9be5e7a2b783994bfb575c1ad
Vaclav Sisl <vaclav.sisl@gmail.com>
2020-05-25 23:31:49 +0200

    removing unused imports

--- dash_app/pages/home_callbacks.py
+++ dash_app/pages/home_callbacks.py
@@ -1,10 +1,4 @@
-from dash.dependencies import Input, Output, State
-import dash_bootstrap_components as dbc
-import dash_html_components as html
-import dash_core_components as dcc
-from dash.exceptions import PreventUpdate
-from dash_table.Format import Format, Scheme, Sign, Symbol
-import pandas as pd
+from dash.dependencies import Input, Output
 import plotly.express as px
 
 from dash_app.app import app, districts, dff
