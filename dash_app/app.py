bed243e9643f33bd3e0265a001d39bf2d1a54952
Vaclav Sisl <vaclav.sisl@gmail.com>
2020-05-30 00:30:00 +0200

    added google tag manager snippets

--- dash_app/app.py
+++ dash_app/app.py
@@ -33,12 +33,23 @@ if apply_ga:
     
                 gtag('config', 'UA-167323389-1', { 'anonymize_ip': true });
             </script>
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P5FMPH3');</script>
+            <!-- End Google Tag Manager -->
             {%metas%}
             <title>{%title%}</title>
             {%favicon%}
             {%css%}
         </head>
         <body>
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5FMPH3"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
             {%app_entry%}
             <footer>
                 {%config%}
